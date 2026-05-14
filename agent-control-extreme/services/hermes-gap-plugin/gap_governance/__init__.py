72877fa8b928ac0fed0643c60675e5a6074511fa
thePM001 <thePM001@newlisbon.agency>
2026-05-14 22:52:05 +0000

    revert: remove hermes-gap-plugin (pushed without human approval)

--- agent-control-extreme/services/hermes-gap-plugin/gap_governance/__init__.py
+++
@@ -1,8 +0,0 @@
-"""GAP Governance Plugin - Hermes Agent integration."""
-
-from gap_governance.client import GAPClient
-from gap_governance.plugin import GAPGovernancePlugin
-from gap_governance.policy_cache import PolicyCache
-
-__version__ = "0.1.0"
-__all__ = ["GAPClient", "GAPGovernancePlugin", "PolicyCache"]
